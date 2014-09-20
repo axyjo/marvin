d08da20e3ce4688e4ae6215adc3495272b3aa905
JGulbronson <jgulbronson@gmail.com>
2014-09-20 19:50:48 -0400

    Added alpha wolfram

---
+++ pylibs/wolfram.py
@@ -0,0 +1,19 @@
+import requests
+import urllib
+import json
+from bs4 import BeautifulSoup
+
+def wolfram_math(entities, client):
+    print "entities"
+    print entities
+    expression = entities.get('math_expression').get('value')
+    wolfram_url = 'http://api.wolframalpha.com/v2/query?appid=HJ95HE-4J53RPLY6T&input='
+    return_format = '&format=plaintext'
+    answer = requests.get(wolfram_url + urllib.quote(expression) + return_format)
+    soup = BeautifulSoup(answer.text)
+    try:
+        answer = soup.queryresult.find_all('pod', title='Result')[0].find(
+                'plaintext').getText()
+    except:
+        return "Sorry, I didn't catch that"
+    return answer
