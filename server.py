19e6e79154cd18572eacd21e05cda33a8b386c49
Akshay Joshi <me@akshayjoshi.com>
2014-09-20 07:52:14 -0400

    TWILIOOOOO!

--- server.py
+++ server.py
@@ -1,13 +1,27 @@
 from flask import Flask
+from flask import request
 from flask import jsonify
 from flask import render_template
 import requests
 import json
 import mongoengine as me
+import twilio.twiml
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
 me.connect('jarvis')
+callers = {}
+
+affirmative = [
+    "Roger that.",
+    "Gotcha. Gonna get on that."
+]
+
+negative = [
+    "I couldn't quite catch that.",
+    "Say what?",
+    "Could you repeat that?"
+]
 
 @app.route("/")
 def landing_page():
@@ -24,6 +38,54 @@ def uber_eta():
             )
     print result.text
     return jsonify(result.json())
+
+@app.route('/callback/twilio/onCall')
+def on_call():
+    resp = twilio.twiml.Response()
+    resp.say("Hello, this is your Robot.")
+    resp.record(
+            action='/callback/twilio/onRecord',
+            method='GET',
+            playBeep=False,
+            maxLength=10,
+            timeout=2
+    )
+    return str(resp)
+
+
+@app.route('/callback/twilio/onRecord')
+def on_record():
+    if request.values.get('Digits', None) == 'hangup':
+        return ''
+
+    url = request.values.get('RecordingUrl', None)
+    resp = twilio.twiml.Response()
+
+    if url == None:
+        resp.hangup()
+        return str(resp)
+
+    waveform_request = requests.get(url)
+    wit_response = requests.post(
+            url='https://api.wit.ai/speech?v=20140920',
+            data=waveform_request.content,
+            headers={
+                'Authorization': 'Bearer 5QX3F2PX2RTQI2DVWRLYV7VXVARO767B',
+                'Content-Type': 'audio/wav'
+            }
+    )
+    print wit_response.text
+
+    resp.say("Roger that.")
+    resp.record(
+            action='/callback/twilio/onRecord',
+            method='GET',
+            playBeep=False,
+            maxLength=10,
+            timeout=2
+    )
+    return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
