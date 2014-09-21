c5d0d8d353e3d3891d39a3118e90bf755aade122
Akshay Joshi <me@akshayjoshi.com>
2014-09-20 20:03:44 -0400

    Math fixes.

--- pylibs/wolfram.py
+++ pylibs/wolfram.py
@@ -6,7 +6,11 @@ from bs4 import BeautifulSoup
 def wolfram_math(entities, client):
     print "entities"
     print entities
-    expression = entities.get('math_expression').get('value')
+    expression = entities.get('math_expression')
+    if (not isinstance(expression, dict)):
+        expression = expression[0]
+    expression = expression.get('value')
+
     wolfram_url = 'http://api.wolframalpha.com/v2/query?appid=HJ95HE-4J53RPLY6T&input='
     return_format = '&format=plaintext'
     answer = requests.get(wolfram_url + urllib.quote(expression) + return_format)
@@ -15,5 +19,9 @@ def wolfram_math(entities, client):
         answer = soup.queryresult.find_all('pod', title='Result')[0].find(
                 'plaintext').getText()
     except:
-        return "Sorry, I didn't catch that"
+        try:
+            answer = soup.queryresult.find_all('pod', title='Exact result')[0].find(
+                'plaintext').getText()
+        except:
+            return "Sorry, I didn't catch that"
     return answer
